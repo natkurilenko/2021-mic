aee8965014ecd332d63211b3b2e023455c0f0b6f
JJosh Granek <josh@duke.edu>
2021-05-30 23:35:50 -0400

    run_singularity_jupyter.sh: switch to docker image from miccourse organization

--- run_singularity/run_singularity_jupyter.sh
+++ run_singularity/run_singularity_jupyter.sh
@@ -4,4 +4,4 @@ set -u
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source ${SCRIPT_DIR}/setup.sh
 
-singularity exec $BIND_ARGS docker://granek/jupyter-mic-2021:latest /usr/local/bin/start.sh jupyter notebook --ip=0.0.0.0 --no-browser
+singularity exec $BIND_ARGS docker://miccourse/jupyter-mic-2021:latest /usr/local/bin/start.sh jupyter notebook --ip=0.0.0.0 --no-browser

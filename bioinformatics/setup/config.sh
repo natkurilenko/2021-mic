2970abff524f3cb27ecd431a56e61f272a79e810
JJosh Granek <josh@duke.edu>
2021-05-30 23:28:36 -0400

    trim_adapters.Rmd: New!

--- bioinformatics/setup/config.sh
+++ bioinformatics/setup/config.sh
@@ -18,7 +18,9 @@ set -u
 # source("common_config.r")
 echo "NEED TO SHARE VARS BETWEEN R AND BASH!!"
 
-GENOME_DIR="/data/genome"
+DATA_DIR="/data"
+
+GENOME_DIR="${DATA_DIR}/genome"
 # FASTA_GZ="${GENOME_DIR}/GRCm39.primary_assembly.genome.fa.gz"
 # GFF_GZ="${GENOME_DIR}/gencode.vM27.primary_assembly.annotation.gff3.gz"
 
@@ -26,3 +28,7 @@ FASTA_PATH="${GENOME_DIR}/GRCm39.primary_assembly.genome.fa"
 GFF_PATH="${GENOME_DIR}/gencode.vM27.primary_assembly.annotation.gff3"
 
 TOTAL_THREADS=80
+
+OUT_DIR="/workspace"
+RAW_FASTQ_DIR="${DATA_DIR}/sra_data"
+TRIM_DIR="${OUT_DIR}/trimmed_fastq"

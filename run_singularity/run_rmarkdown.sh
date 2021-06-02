4564e1bdbe97d7e18ccc8f40098485910d42c0c3
JJosh Granek <josh@duke.edu>
2021-06-02 14:38:52 -0400

    renamed:    run_full_analysis.sh -> run_rmarkdown.sh

--- run_singularity/run_rmarkdown.sh
+++ run_singularity/run_rmarkdown.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# +
 set -u
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source ${SCRIPT_DIR}/setup.sh
 
+RMARKDOWN="${1}
+# -
+
 KNIT_OUT='/workspace/knit_output_html'
 
-singularity exec $BIND_ARGS $SINGULARITY_IMAGE Rscript -e "rmarkdown::render('${SCRIPT_DIR}/bioinformatics/setup/download_data_sra.Rmd', output_dir='${KNIT_OUT}')"
+singularity exec $BIND_ARGS $SINGULARITY_IMAGE Rscript -e "rmarkdown::render('${RMARKDOWN}', output_dir='${KNIT_OUT}')"

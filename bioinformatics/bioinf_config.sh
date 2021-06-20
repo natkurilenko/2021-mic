83d08e3b816d06f0c3ff10be868f0c033c427893
JJosh Granek <josh@duke.edu>
2021-06-20 19:05:03 -0400

    bioinf_config.sh: made OUTBASE variable

--- bioinformatics/bioinf_config.sh
+++ bioinformatics/bioinf_config.sh
@@ -17,45 +17,45 @@
 set -u
 
 DATA_DIR="/data"
+OUTBASE="$HOME/scratch"
 RAW_FASTQ_DIR="$DATA_DIR/sra_data"
-
-OUT_DIR=$HOME/scratch/bioinf_intro
-QC=$OUT_DIR/qc_output
-
 
 TOTAL_THREADS=20
 TOTAL_RAM=100000000000
 
+
+OUT_DIR=$OUTBASE/bioinf_intro
+QC=$OUT_DIR/qc_output
 ADAPTER_FASTA="${OUT_DIR}/adapter.fasta"
 TRIM_DIR="${OUT_DIR}/trimmed_fastq"
 STAROUT_DIR="${OUT_DIR}/starout"
 
 # Paired-end
-PE_OUT_DIR=$HOME/scratch/bioinf_pe_analysis
+PE_OUT_DIR=$OUTBASE/bioinf_pe_analysis
 PE_QC=$PE_OUT_DIR/qc_output
 PE_TRIM_DIR="${PE_OUT_DIR}/trimmed_fastq"
 PE_STAROUT_DIR="${PE_OUT_DIR}/starout"
 
 # Pipeline
-PIPE_OUT_DIR=$HOME/scratch/bioinf_pipeline
+PIPE_OUT_DIR=$OUTBASE/bioinf_pipeline
 PIPE_QC=$PIPE_OUT_DIR/qc_output
 PIPE_TRIM_DIR="${PIPE_OUT_DIR}/trimmed_fastq"
 PIPE_STAROUT_DIR="${PIPE_OUT_DIR}/starout"
 
 # Loops
-LOOP_OUT_DIR=$HOME/scratch/bioinf_loop
+LOOP_OUT_DIR=$OUTBASE/bioinf_loop
 LOOP_QC=$LOOP_OUT_DIR/qc_output
 LOOP_TRIM_DIR="${LOOP_OUT_DIR}/trimmed_fastq"
 LOOP_STAROUT_DIR="${LOOP_OUT_DIR}/starout"
 
 # Multiple FASTQs
-MULTI_OUT_DIR=$HOME/scratch/bioinf_multifastqs
+MULTI_OUT_DIR=$OUTBASE/bioinf_multifastqs
 MULTI_QC=$MULTI_OUT_DIR/qc_output
 MULTI_TRIM_DIR="${MULTI_OUT_DIR}/trimmed_fastq"
 MULTI_STAROUT_DIR="${MULTI_OUT_DIR}/starout"
 
 # Glob Loop
-GLOB_OUT_DIR=$HOME/scratch/bioinf_glob
+GLOB_OUT_DIR=$OUTBASE/bioinf_glob
 GLOB_QC=$GLOB_OUT_DIR/qc_output
 GLOB_TRIM_DIR="${GLOB_OUT_DIR}/trimmed_fastq"
 GLOB_STAROUT_DIR="${GLOB_OUT_DIR}/starout"
